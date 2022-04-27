01fd4bb20e6aedaf9ae430ce593ad3f8f535a7ab
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-04-27 04:12:49 +0300

    ibrahimbayramli

---
+++ IsimlendirmeKurallari/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Camel Case (metodlar, de?i?kenler)
+        String camelCaseStringim="Camel Case";
+
+        // Upper Camel Case - Pascal Case (s?n?flar)
+        String PascalCaseStringim="Pascal Case - Upper Camel Case";
+
+        //Snake Case
+        String snake_case_stringim="Snake Case";
+
+        //Screaming Snake Case
+        String SCREAMING_SNAKE_CASE_STRINGIM="Screaming Snake Case";
+
+
+    }
+}
