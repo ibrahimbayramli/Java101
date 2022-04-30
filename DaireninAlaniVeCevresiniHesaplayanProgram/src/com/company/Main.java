0ccd3d6714dc08dc4c23be53d743cd1f43f00d41
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-04-30 15:00:54 +0300

    ibrahimbayramli

---
+++ DaireninAlaniVeCevresiniHesaplayanProgram/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final float pi = 3.14f;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Yaricap giriniz: ");
+        float yariCap = scan.nextFloat();
+
+        float alan = pi * yariCap * yariCap;
+        float cevre = 2 * pi * yariCap;
+
+        System.out.println("Dairenin alani: " + alan);
+        System.out.println("Dairenin cevresi: " + cevre);
+
+
+    }
+}
