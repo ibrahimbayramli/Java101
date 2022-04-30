b29137df57b4e7fe892d517dd334c5e243dc167d
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-04-30 13:25:55 +0300

    ibrahimbayramli

---
+++ KDVTutarHesaplamaProgrami/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+	    System.out.print("Ürün tutarini giriniz: ");
+        float tutar=scan.nextFloat();
+        float odeme;
+
+        if(tutar>=0 && tutar<=1000) odeme=tutar*1.18f;
+        else odeme=tutar*1.08f;
+
+        System.out.println("Ödenmesi gereken tutar: " + odeme);
+
+
+    }
+}
