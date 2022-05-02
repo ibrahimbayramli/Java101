31e0f3824b833411a7cbeb978aab7625bf46240c
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-05-02 05:11:00 +0300

    HelloWorld

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+        // Patika JAVA 101
+        System.out.println("Hello World!");
     }
 }
