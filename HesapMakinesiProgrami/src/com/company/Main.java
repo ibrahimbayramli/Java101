b14f5eba1aa5211e3723878a4b9c00f519ae2cd4
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-05-02 05:11:54 +0300

    HesapMakinesi

--- HesapMakinesiProgrami/src/com/company/Main.java
+++ HesapMakinesiProgrami/src/com/company/Main.java
@@ -5,38 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Yapmak istediginiz islemi giriniz:");
-	    System.out.println("1- Toplama");
-	    System.out.println("2- Cikarma");
-	    System.out.println("3- Carpma");
-	    System.out.println("4- Bolme");
-    System.out.println();
+        // Patika JAVA 101
+        System.out.println("Yapmak istediginiz islemi giriniz:");
+        System.out.println("1- Toplama");
+        System.out.println("2- Cikarma");
+        System.out.println("3- Carpma");
+        System.out.println("4- Bolme");
+        System.out.println();
 
-    Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        int secim=scan.nextInt();
-    System.out.print("1. sayiyi giriniz: ");
-    int sayi1=scan.nextInt();
-    System.out.print("2. sayiyi giriniz: ");
-    int sayi2=scan.nextInt();
-    float sonuc=0;
+        int secim = scan.nextInt();
+        System.out.print("1. sayiyi giriniz: ");
+        int sayi1 = scan.nextInt();
+        System.out.print("2. sayiyi giriniz: ");
+        int sayi2 = scan.nextInt();
+        float sonuc = 0;
 
-    switch (secim) {
-        case 1:
-            sonuc=sayi1+sayi2;
-            break;
-        case 2:
-            sonuc=sayi1-sayi2;
-            break;
-        case 3:
-            sonuc=sayi1*sayi2;
-            break;
-        case 4:
-            sonuc=sayi1/sayi2;
-            break;
-    }
-    System.out.println("Sonuc: "+sonuc);
-
+        switch (secim) {
+            case 1:
+                sonuc = sayi1 + sayi2;
+                break;
+            case 2:
+                sonuc = sayi1 - sayi2;
+                break;
+            case 3:
+                sonuc = sayi1 * sayi2;
+                break;
+            case 4:
+                sonuc = sayi1 / sayi2;
+                break;
+        }
+        System.out.println("Sonuc: " + sonuc);
 
 
     }
