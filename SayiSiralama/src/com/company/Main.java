072ef4a8ac93ab53d48292ffb0e550fe69e4772d
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-05-09 01:41:29 +0300

    Siralama

--- SayiSiralama/src/com/company/Main.java
+++ SayiSiralama/src/com/company/Main.java
@@ -6,17 +6,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    int sayi1,sayi2,sayi3,enBuyuk;
-        Scanner scan=new Scanner(System.in);
+        int sayi1, sayi2, sayi3, rakam1=0, rakam2=0, rakam3=0;
+        Scanner scan = new Scanner(System.in);
+
         System.out.print("Birinci sayıyı giriniz: ");
-        sayi1=scan.nextInt();
+        sayi1 = scan.nextInt();
+
         System.out.print("İkinci sayıyı giriniz: ");
-        sayi2=scan.nextInt();
+        sayi2 = scan.nextInt();
+
         System.out.print("Üçüncü sayıyı giriniz: ");
-        sayi3=scan.nextInt();
-        if(sayi1>sayi2 && sayi1>sayi3) enBuyuk=sayi1;
-        else if (sayi2>sayi1 && sayi2>sayi3) enBuyuk=sayi2;
-        else enBuyuk=sayi3;
-        System.out.println("En büyük sayı: "+enBuyuk);
+        sayi3 = scan.nextInt();
+
+        if (sayi1 > sayi2 && sayi1 > sayi3) {
+            rakam1 = sayi1;
+            if (sayi2 > sayi3) {
+                rakam2 = sayi2;
+                rakam3 = sayi3;
+            }
+        } else if (sayi2 > sayi1 && sayi2 > sayi3) {
+            rakam1 = sayi2;
+            if (sayi1 > sayi3) {
+                rakam2 = sayi1;
+                rakam3 = sayi3;
+            }
+        } else {
+            rakam1 = sayi3;
+            if (sayi2 > sayi1) {
+                rakam2 = sayi2;
+                rakam3 = sayi1;
+            }
+        }
+
+        System.out.println("Sıralama: " + rakam3 + " " + rakam2 + " " + rakam1);
     }
 }
