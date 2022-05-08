df2bd6355637458497849a05c6790d3af49e2894
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-05-08 14:45:14 +0300

    Kullanıcı girişi

--- KullaniciGirisi/src/com/company/Main.java
+++ KullaniciGirisi/src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Patika JAVA 101
+
+        String userName,password;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Kullanıcı adınız: ");
+        userName=scan.nextLine();
+        System.out.print("Şifreniz: ");
+        password=scan.nextLine();
+
+        if(userName.equals("patika") && password.equals("java123")) System.out.println("Giriş yaptınız.");
+        else System.out.println("Bilgileriniz hatalı.");
 
     }
 }
