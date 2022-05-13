93c1d4a36303153eaafddf2dcbab9f3945f11f81
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-05-14 02:21:38 +0300

    Cin Zodyagi Hesaplama

---
+++ CinZodyagıHesaplama/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Doðum yýlýnýzý giriniz: ");
+        int yil=scan.nextInt();
+
+        int kalan=yil%12;
+
+        switch (kalan){
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavþan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yýlan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+
+        }
+    }
+}
