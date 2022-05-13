7431dba5eeeca4f4d7a713e4434c3cff2623891a
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2022-05-14 02:28:33 +0300

    Artik Yil Hesaplama

---
+++ ArtikYilHesaplma/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Yýl giriniz: ");
+        int yil=scan.nextInt();
+        if(yil%100==0){
+            if(yil%400==0){
+                System.out.println(yil+" bir artýk yýldýr.");
+            }else {
+                System.out.println(yil+" bir artýk yýl deðildir.");
+            }
+        }else{
+            if(yil%4==0){
+                System.out.println(yil+" bir artýk yýldýr.");
+            }else {
+                System.out.println(yil+" bir artýk yýl deðildir.");
+            }
+        }
+
+    }
+}
