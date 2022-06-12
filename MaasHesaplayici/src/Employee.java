7f961d7ba11d0299dd75967ad75e8aad1d8e082f
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-06-13 01:43:36 +0300

    odev

---
+++ MaasHesaplayici/src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+    Employee(String name,double salary,int workHours,int hireYear){
+        this.name=name;
+        this.salary=salary;
+        this.workHours=workHours;
+        this.hireYear=hireYear;
+    }
+}
