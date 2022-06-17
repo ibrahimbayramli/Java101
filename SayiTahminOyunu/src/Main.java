3c203822301ad5eed11397c268831a33c21a36bc
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-06-17 22:33:45 +0300

    Sayi tahmin oyunu

---
+++ SayiTahminOyunu/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        double rnd=Math.floor(Math.random()*101);
+        
+        System.out.print("Tahmininizi giriniz: ");
+        int tahmin=scan.nextInt();
+        int sayac=3;
+
+        if(tahmin==rnd){
+            System.out.println("Tebrikler!!!");
+        }else {
+            while (sayac>0){
+                System.out.print("Tekrar bir tahminde bulununuz: ");
+                tahmin= scan.nextInt();
+                if (tahmin==rnd){
+                    System.out.println("Tebrikler!!!");
+                    break;
+                }
+                sayac--;
+            }
+
+        }
+        if (sayac==0){
+            System.out.println("Kaybettiniz!!");
+        }
+
+
+    }
+}
